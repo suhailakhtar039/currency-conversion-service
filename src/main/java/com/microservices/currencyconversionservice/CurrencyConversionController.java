1a46a54797e3b246f37a50650d3e30ce966d7d81
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-24 01:04:55 +0530

    linked to currency exchange

--- src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,25 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable(value = "from") String from,
 			@PathVariable(value = "to") String to, @PathVariable(value = "quantity") BigDecimal quantity) {
-				
+
 		HashMap<String, String> uriVariables = new HashMap<>();
-		uriVariables.put(from, "from");
-		uriVariables.put(to,"to");
-		new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
-				CurrencyConversion.class,uriVariables);
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				CurrencyConversion.class, uriVariables);
+
 		
-		return new CurrencyConversion(1001L,from,to,quantity,BigDecimal.ONE,BigDecimal.ONE,"");
+		CurrencyConversion currencyConversion = responseEntity.getBody();
 		
+		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, 
+				currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment());
 
 	}
 
